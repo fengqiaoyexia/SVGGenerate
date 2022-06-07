44e3805af17619f22a219ffe97fbac15d1cf1d80
Luoy <Luoy@DESKTOP-QQQ18QG>
2022-06-07 22:42:44 +0800

    1.Add container for original and duplicate noe

--- SvgGenerate.cs
+++ SvgGenerate.cs
@@ -16,6 +16,8 @@ namespace SingleToDuplciate
         private int _svgHeight = 361;
         private int _originalWidth;
         private int _originalHeight;
+        private XmlElement _originalContainer;
+        private XmlElement _duplicateContainer;
 
         public SvgGenerate(Stream stream, int svgWidth, int svgHeight)
         {
@@ -37,10 +39,26 @@ namespace SingleToDuplciate
 
             UpdateSvgNode(root);
 
+            AddMaskLayer(root);
+
+            AddContainerNode(root);
+
             var parameterNode = root.SelectSingleNode($"//{_manager.GetNamespacesInScope(XmlNamespaceScope.Local).ToList().FirstOrDefault(it => it.Key.ToLower().Contains("cpdts")).Key}:parameters", _manager);
             if (parameterNode != null)
             {
                 UpdateParametersNode(parameterNode);
+            }
+
+            var defsNode = root.SelectSingleNode("//svg:defs", _manager);
+            if (defsNode != null)
+            {
+                UpdateDefsNode(defsNode);
+            }
+
+            var clipPathNode = root.SelectSingleNode("//svg:clipPath", _manager);
+            if (clipPathNode != null)
+            {
+                UpdateClipPathNode(clipPathNode);
             }
 
             var gImageNodes = root.SelectNodes("//*[@cpbv:class='image']", _manager);
@@ -56,7 +74,7 @@ namespace SingleToDuplciate
                 UpdateGTextNodes(gTextNodes.Cast<XmlNode>().ToList());
             }
 
-            AddMaskLayer(root);
+
 
             _xmlDoc.Save(outStream);
             outStream.Position = 0;
@@ -94,8 +112,10 @@ namespace SingleToDuplciate
                 var duplicateNode = GenerateImageNode(node, false, index);
 
                 parentNode.RemoveChild(node);
-                parentNode.AppendChild(orginalNode);
-                parentNode.AppendChild(duplicateNode);
+                _originalContainer.AppendChild(orginalNode);
+                _duplicateContainer.AppendChild(duplicateNode);
+                //parentNode.AppendChild(orginalNode);
+                //parentNode.AppendChild(duplicateNode);
             }
         }
 
@@ -109,9 +129,106 @@ namespace SingleToDuplciate
                 var duplicateNode = GenerateTextNode(node, false, index);
 
                 parentNode.RemoveChild(node);
-                parentNode.AppendChild(orginalNode);
-                parentNode.AppendChild(duplicateNode);
+                _originalContainer.AppendChild(orginalNode);
+                _duplicateContainer.AppendChild(duplicateNode);
+                //parentNode.AppendChild(orginalNode);
+                //parentNode.AppendChild(duplicateNode);
             }
+        }
+
+        private void UpdateImageNodeId(XmlNode node, int offset)
+        {
+            var pattern = @"\d+";
+            var gIDAttribute = node.Attributes["id"];
+            if (gIDAttribute != null)
+            {
+                var gID = gIDAttribute.Value;
+                var gID_num = Convert.ToInt32(Regex.Match(gID, pattern).Groups[0].Value);
+                gIDAttribute.Value = gID.Replace($"canvasFront_image_{gID_num}", $"canvasFront_image_{gID_num + offset}");
+            }
+
+            var clipPathID = "";
+            var clipPathID_num = 0;
+            if (node.SelectSingleNode("//*[contains(@id,'CLIPPATH')]", _manager) != null)
+            {
+                var clipPathIDAttribute = node.SelectSingleNode("//*[contains(@id,'CLIPPATH')]", _manager).Attributes["id"];
+                if (clipPathIDAttribute != null)
+                {
+                    clipPathID = clipPathIDAttribute.Value;
+                    clipPathID_num = Convert.ToInt32(Regex.Match(clipPathID, pattern).Groups[0].Value);
+                    clipPathIDAttribute.Value = clipPathID.Replace($"CLIPPATH_{clipPathID_num}", $"CLIPPATH_{clipPathID_num + offset}");
+                }
+            }
+
+            var imageIDAttribute = node.SelectSingleNode("//*[@xlink:href]", _manager).Attributes["id"];
+            if (imageIDAttribute != null)
+            {
+                var imageID = imageIDAttribute.Value;
+                var imageID_num = Convert.ToInt32(Regex.Match(imageID, pattern).Groups[0].Value);
+                imageIDAttribute.Value = imageID.Replace($"i{imageID_num}", $"i{imageID_num + offset}");
+            }
+
+            var clipPathAttribute = node.Attributes["clip-path"];
+            if (clipPathAttribute != null)
+            {
+                clipPathAttribute.Value = $"url(#{clipPathID.Replace($"CLIPPATH_{clipPathID_num}", $"CLIPPATH_{clipPathID_num + offset}")})";
+            }
+        }
+
+        private void UpdateTextNodeId(XmlNode node, int offset, double sizeScale)
+        {
+            var pattern = @"\d+";
+            var gIDAttribute = node.Attributes["id"];
+            if (gIDAttribute != null)
+            {
+                var gID = node.Attributes["id"].Value;
+                var gID_num = Convert.ToInt32(Regex.Match(gID, pattern).Groups[0].Value);
+                gIDAttribute.Value = gID.Replace($"{gID_num}", $"{gID_num + offset}");
+            }
+
+            if (node.SelectSingleNode("//svg:text", _manager) != null)
+            {
+                var textIDAttribute = node.SelectSingleNode("//svg:text", _manager).Attributes["id"];
+                if (textIDAttribute != null)
+                {
+                    var textID = textIDAttribute.Value;
+                    var textID_num = Convert.ToInt32(Regex.Match(textID, pattern).Groups[0].Value);
+                    textIDAttribute.Value = textID.Replace($"{textID_num}", $"{textID_num + offset}");
+                }
+            }
+
+            // textNode need to add a offset for <tspan> node's x attribute, offset = 7/10 * font-size
+            var tspanNodes = node.SelectNodes("//svg:tspan", _manager);
+            if (tspanNodes != null && tspanNodes.Count > 0)
+            {
+                var textFontSize = Convert.ToInt32(node.SelectSingleNode("//*[@font-size]").Attributes["font-size"].Value);
+                for (int i = 0; i < tspanNodes.Count; i++)
+                {
+                    var tspanNode = tspanNodes[i];
+                    var tspanX = Convert.ToDouble(tspanNode.Attributes["x"].Value);
+                    var tspanY = Convert.ToDouble(tspanNode.Attributes["y"].Value);
+
+                    if (tspanNodes.Count == i + 1 && tspanNodes.Count > 1)
+                    {
+                        tspanNode.Attributes["x"].Value = (tspanX + (4.0 / 10.0 * textFontSize)).ToString();
+                    }
+                    else
+                    {
+                        tspanNode.Attributes["x"].Value = (tspanX + (7.0 / 10.0 * textFontSize)).ToString();
+                    }
+                    tspanNode.Attributes["y"].Value = (tspanY * sizeScale).ToString();
+                }
+            }
+        }
+
+        private void UpdateDefsNode(XmlNode node)
+        {
+
+        }
+
+        private void UpdateClipPathNode(XmlNode node)
+        {
+
         }
 
         private List<XmlNode> GetParameterNodes(List<XmlNode> nodeList)
@@ -269,117 +386,52 @@ namespace SingleToDuplciate
             return tmpNodes;
         }
 
-        private void UpdateImageNodeId(XmlNode node, int offset)
+        private XmlNode GenerateMaskChildNode(string id, double x, double y, double width, double height)
         {
-            var pattern = @"\d+";
-            var gIDAttribute = node.Attributes["id"];
-            if (gIDAttribute != null)
-            {
-                var gID = gIDAttribute.Value;
-                var gID_num = Convert.ToInt32(Regex.Match(gID, pattern).Groups[0].Value);
-                gIDAttribute.Value = gID.Replace($"canvasFront_image_{gID_num}", $"canvasFront_image_{gID_num + offset}");
-            }
-
-            var clipPathID = "";
-            var clipPathID_num = 0;
-            if (node.SelectSingleNode("//*[contains(@id,'CLIPPATH')]", _manager) != null)
-            {
-                var clipPathIDAttribute = node.SelectSingleNode("//*[contains(@id,'CLIPPATH')]", _manager).Attributes["id"];
-                if (clipPathIDAttribute != null)
-                {
-                    clipPathID = clipPathIDAttribute.Value;
-                    clipPathID_num = Convert.ToInt32(Regex.Match(clipPathID, pattern).Groups[0].Value);
-                    clipPathIDAttribute.Value = clipPathID.Replace($"CLIPPATH_{clipPathID_num}", $"CLIPPATH_{clipPathID_num + offset}");
-                }
-            }
-
-            var imageIDAttribute = node.SelectSingleNode("//*[@xlink:href]", _manager).Attributes["id"];
-            if (imageIDAttribute != null)
-            {
-                var imageID = imageIDAttribute.Value;
-                var imageID_num = Convert.ToInt32(Regex.Match(imageID, pattern).Groups[0].Value);
-                imageIDAttribute.Value = imageID.Replace($"i{imageID_num}", $"i{imageID_num + offset}");
-            }
-
-            var clipPathAttribute = node.Attributes["clip-path"];
-            if (clipPathAttribute != null)
-            {
-                clipPathAttribute.Value = $"url(#{clipPathID.Replace($"CLIPPATH_{clipPathID_num}", $"CLIPPATH_{clipPathID_num + offset}")})";
-            }
-        }
-
-        private void UpdateTextNodeId(XmlNode node, int offset, double sizeScale)
-        {
-            var pattern = @"\d+";
-            var gIDAttribute = node.Attributes["id"];
-            if (gIDAttribute != null)
-            {
-                var gID = node.Attributes["id"].Value;
-                var gID_num = Convert.ToInt32(Regex.Match(gID, pattern).Groups[0].Value);
-                gIDAttribute.Value = gID.Replace($"{gID_num}", $"{gID_num + offset}");
-            }
-
-            if (node.SelectSingleNode("//svg:text", _manager) != null)
-            {
-                var textIDAttribute = node.SelectSingleNode("//svg:text", _manager).Attributes["id"];
-                if (textIDAttribute != null)
-                {
-                    var textID = textIDAttribute.Value;
-                    var textID_num = Convert.ToInt32(Regex.Match(textID, pattern).Groups[0].Value);
-                    textIDAttribute.Value = textID.Replace($"{textID_num}", $"{textID_num + offset}");
-                }
-            }
-
-            // textNode need to add a offset for <tspan> node's x attribute, offset = 7/10 * font-size
-            var tspanNodes = node.SelectNodes("//svg:tspan", _manager);
-            if (tspanNodes != null && tspanNodes.Count > 0)
-            {
-                var textFontSize = Convert.ToInt32(node.SelectSingleNode("//*[@font-size]").Attributes["font-size"].Value);
-                for (int i = 0; i < tspanNodes.Count; i++)
-                {
-                    var tspanNode = tspanNodes[i];
-                    var tspanX = Convert.ToDouble(tspanNode.Attributes["x"].Value);
-                    var tspanY = Convert.ToDouble(tspanNode.Attributes["y"].Value);
-
-                    if (tspanNodes.Count == i + 1 && tspanNodes.Count > 1)
-                    {
-                        tspanNode.Attributes["x"].Value = (tspanX + (4.0 / 10.0 * textFontSize)).ToString();
-                    }
-                    else
-                    {
-                        tspanNode.Attributes["x"].Value = (tspanX + (7.0 / 10.0 * textFontSize)).ToString();
-                    }
-                    tspanNode.Attributes["y"].Value = (tspanY * sizeScale).ToString();
-                }
-            }
+            var mask = _xmlDoc.CreateElement("mask", _xmlDoc.DocumentElement.NamespaceURI);
+            mask.SetAttribute("id", id);
+            mask.SetAttribute("x", 0.ToString());
+            mask.SetAttribute("y", 0.ToString());
+            var mask_rect = _xmlDoc.CreateElement("rect", _xmlDoc.DocumentElement.NamespaceURI);
+            mask_rect.SetAttribute("x", x.ToString());
+            mask_rect.SetAttribute("y", y.ToString());
+            mask_rect.SetAttribute("width", height.ToString());
+            mask_rect.SetAttribute("height", height.ToString());
+            mask_rect.SetAttribute("fill", "white");
+            mask.AppendChild(mask_rect);
+            return mask;
         }
 
         private void AddMaskLayer(XmlNode node)
         {
-            var mask_g = _xmlDoc.CreateElement("g", _xmlDoc.DocumentElement.NamespaceURI);
-            if (mask_g.Attributes["xmlns"] != null)
+            var defs = _xmlDoc.CreateElement("defs", _xmlDoc.DocumentElement.NamespaceURI);
+            if (defs.Attributes["xmlns"] != null)
             {
-                mask_g.RemoveAttribute("xmlns");
+                defs.RemoveAttribute("xmlns");
             }
-            var offset1 = (_svgWidth * 1.0 / 2.0);
-            var offset2 = ((_svgWidth * 1.0 / 2.0 - _svgHeight * 1.0) / 2.0);
-            mask_g.AppendChild(GenerateMaskChildNode("mask_left_left", 0, 0, offset2, _svgHeight));
-            mask_g.AppendChild(GenerateMaskChildNode("mask_left_right", offset2 + _svgHeight, 0, offset2, _svgHeight));
-            mask_g.AppendChild(GenerateMaskChildNode("mask_right_left", offset1, 0, offset2, _svgHeight));
-            mask_g.AppendChild(GenerateMaskChildNode("mask_right_right", offset1 + offset2 + _svgHeight, 0, offset2, _svgHeight));
-            node.AppendChild(mask_g);
+            defs.AppendChild(GenerateMaskChildNode("originalMask", 530.5, 0, 361, 361));
+            defs.AppendChild(GenerateMaskChildNode("duplicateMask", 69.5, 0, 361, 361));
+            node.AppendChild(defs);
         }
 
-        private XmlNode GenerateMaskChildNode(string id, double x, double y, double width, double height)
+        private void AddContainerNode(XmlNode node)
         {
-            var rect = _xmlDoc.CreateElement("rect", _xmlDoc.DocumentElement.NamespaceURI);
-            rect.SetAttribute("id", id);
-            rect.SetAttribute("x", x.ToString());
-            rect.SetAttribute("y", y.ToString());
-            rect.SetAttribute("width", width.ToString());
-            rect.SetAttribute("height", height.ToString());
-            rect.SetAttribute("fill", "white");
-            return rect;
+            _originalContainer = _xmlDoc.CreateElement("g", _xmlDoc.DocumentElement.NamespaceURI);
+            _originalContainer.SetAttribute("mask", "url(#originalMask)");
+            if (_originalContainer.Attributes["xmlns"] != null)
+            {
+                _originalContainer.RemoveAttribute("xmlns");
+            }
+
+            _duplicateContainer = _xmlDoc.CreateElement("g", _xmlDoc.DocumentElement.NamespaceURI);
+            _duplicateContainer.SetAttribute("mask", "url(#duplicateMask)");
+            if (_duplicateContainer.Attributes["xmlns"] != null)
+            {
+                _duplicateContainer.RemoveAttribute("xmlns");
+            }
+
+            node.AppendChild(_originalContainer);
+            node.AppendChild(_duplicateContainer);
         }
 
         private XmlNamespaceManager GetNamespaceManager(XmlDocument xmlDoc)
